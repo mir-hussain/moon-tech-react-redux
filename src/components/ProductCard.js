020ced7d5ca49753fc0e89d58d142cee72201b6b
mir-hussain <mirhussainmurtaza@gmail.com>
2022-11-28 21:27:36 +0600

    first commit

---
+++ src/components/ProductCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
+
+const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'>
+      <div className='h-52 w-52 mx-auto'>
+        <img src={product.image} alt={product.model} />
+      </div>
+      <h1 className='font-bold text-center'>{product.model}</h1>
+      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
+      <div className=' flex-1'>
+        <ul className='space-y-2'>
+          {product.keyFeature.map((feature) => {
+            return (
+              <li key={feature} className='text-sm '>
+                {feature}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className='flex gap-2 mt-5'>
+        <button
+          onClick={() => dispatch({ type: ADD_TO_CART, payload: product })}
+          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+        >
+          Add to cart
+        </button>
+        <button
+          title='Add to wishlist'
+          className='bg-indigo-500  py-1 px-2 rounded-full'
+        >
+          <BiListPlus className='text-white' />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,19 @@
+import { ADD_TO_CART } from "../actionTypes/actionTypes";
+
+const initialState = {
+  cart: [],
+};
+
+const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default productReducer;
